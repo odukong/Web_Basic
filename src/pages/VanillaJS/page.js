24bc6da93a60a44b03e704783f22b9e0686ecc3f
odukong <ohbin1017@gmail.com>
2024-04-09 17:30:40 +0900

    ✨ Dev: [VanillaJS] Sidebar구현
    - navigation을 변형한 버전으로, 좌측에서 -100%~0 을 이동하여 toggle시마다 열고 닫을 수 있는 sidebar 구현
    
    solves: #2

--- src/pages/VanillaJS/page.js
+++ src/pages/VanillaJS/page.js
@@ -4,6 +4,7 @@ import ColorFlipper from "./ColorFlipper/ColorFlipper";
 import Counter from "./Counter/Counter";
 import Reviews from "./Reviews/Reviews";
 import Navigation from "./Navigation/Navigation";
+import Sidebar from "./Sidebar/Sidebar";
 
 function VanillaPage() {
   const navigate = useNavigate();
@@ -17,6 +18,7 @@ function VanillaPage() {
       <button onClick={() => navigate("../counter")}>Counter</button>
       <button onClick={() => navigate("../reviews")}>Reviews</button>
       <button onClick={() => navigate("../navigation")}>Navigation</button>
+      <button onClick={() => navigate("../sidebar")}>Sidebar</button>
     </>
   );
 }
@@ -29,6 +31,7 @@ export default function Page() {
       <Route path="counter/*" element={<Counter />} />
       <Route path="reviews/*" element={<Reviews />} />
       <Route path="navigation/*" element={<Navigation />} />
+      <Route path="sidebar/*" element={<Sidebar />} />
     </Routes>
   );
 }
